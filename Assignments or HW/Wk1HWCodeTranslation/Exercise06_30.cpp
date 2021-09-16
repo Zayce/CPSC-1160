0e5c54688de090cf0fd78dff6d0a80dbb5a2bd77
Zayce <zayced@gmail.com>
2021-09-16 10:27:23 -0700

    Exercise 4 almost done, Some bugs otw

---
+++ Assignments or HW/Wk1HWCodeTranslation/Exercise06_30.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+
+//TODO: WHY THE FUCK DOES C++ BEHAVE DIFFERENTLY IN DIFFERENT COMPILERS
+using namespace std;
+
+int getDice();
+
+int main(){
+    int dice = getDice();
+
+    //You win early if dice value is 7 or 11
+    if (dice == 7 || dice == 11) {
+      cout << " You win" << endl;
+      exit(1);
+    }
+    else if (dice == 2 || dice == 3 || dice == 12) {
+      cout << " You lose" << endl;
+      exit(2);
+    }
+    int point = dice;
+    cout << "point is " << point << endl;
+
+    do{
+      dice = getDice();
+    } while ((dice != 7 ) && (dice == point));
+    
+    if (dice == 7){
+      cout << " You lose" << endl;
+    }
+    else{
+      cout << " You win" << endl;
+    }
+   }
+
+int getDice() {
+  srand(time(0));
+  int i1 = (1 + (rand() % 6));
+  int i2 = (1 + (rand() % 6));
+
+  cout << "You rolled " << i1 << " + " << i2 << " = " << (i1+i2) << endl;
+  return (i1 + i2);
+}
--- Week2Test.cpp
+++ Week2Test.cpp
@@ -5,17 +5,16 @@ using namespace std;
 int main(){
     double product = 1.0;
 
-    //outputs 1.2599e-51%                                                                                                                                                                     
+    /*outputs 1.2599e-51%                                                                                                                                                                     
     for(double i = 5; i <= 123; i += 4){
         product *= ((1.0)/i);
-    }
+    }*/
 
-    /* outputs 1.2599e-51%                                                                                                                                                                       
-    for(double i = 5; i <= 123; i += 4){
+    //outputs 1.2599e-51%                                                                                                                                                                       
+    for(double i = 5; i <= 49; i += 4){
         product *= (i);
     }
     product = 1.0/product;
-    */
                                                                                                                                                                       
     cout << product;
     return 0;
