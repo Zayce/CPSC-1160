2f992fc59ae5c63bf83b8d463dcaaccbdd757e69
Zayce <zayced@gmail.com>
2021-09-24 13:35:01 -0700

    weird

--- Assignments or HW/Wk1HWCodeTranslation/Exercise07_07.cpp
+++ Assignments or HW/Wk1HWCodeTranslation/Exercise07_07.cpp
@@ -5,18 +5,20 @@
 using namespace std;
 
 int main(){
-    srand(time(0));
+    // srand(time(0));
 
-    const int SIZE = 10;
+    // const int SIZE = 10;
 
-    int* counts = new int[SIZE];
+    // int* counts = new int[SIZE];
 
-    for (int i = 0; i < 100; i++) {
-      int value = rand() % 10;
-      counts[value]++;
-    }
+    // for (int i = 0; i < 100; i++) {
+    //   int value = rand() % 10;
+    //   counts[value]++;
+    // }
 
-    for (int i = 0; i < SIZE; i++) {
-        cout << "Count for " << i << " is " << counts[i] << endl;
-    }
+    // for (int i = 0; i < SIZE; i++) {
+    //     cout << "Count for " << i << " is " << counts[i] << endl;
+    // }
+
+
 }
---
+++ Lab/Lab 2/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    // srand(time(0));
+
+    // for(int i = 0; i < 100; i++){
+    //     cout << 5 + (rand() % 51) << ", ";
+    // }
+
+    // cout << endl;
+
+        int pages = 0;
+    int lines = 0;
+    int columns = 0;
+
+        cout << "To create a book, please enter the: " << endl;
+    cout << "Pages: ";
+    cin >> pages;
+    cout << "Hi";
+
+}
