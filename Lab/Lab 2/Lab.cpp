d758349b8b2c27d34f1fe08fad7c63b44941e543
Zayce <zayced@gmail.com>
2021-09-24 20:43:48 -0700

    Lab2 working with some required comments added

--- Lab/Lab 2/Lab.cpp
+++ Lab/Lab 2/Lab.cpp
@@ -13,7 +13,8 @@
 
 using namespace std;
 int*** create3DArray(int, int, int);
-void print3DArray(int***,int, int, int);
+int randomValueGenerator();
+void display3DArray(int***,int, int, int);
 void delete3DArray(int***,int, int);
 void swapArr(int***, int, int);
 int getPageSum(int***, int, int, int);
@@ -31,47 +32,81 @@ int main(){
 
     //Asks for input to put value in..
     cout << "To create a book, please enter the number of: " << endl;
-    cout << "Pages =  ";
+    cout << "Pages = ";
     cin >> pages;
-    cout << "Lines =  ";
+    cout << "Lines = ";
     cin >> lines;
-    cout << "Columns =  ";
+    cout << "Columns = ";
     cin >> columns;
 
+
+    //Creates 3D Array
     int***book = create3DArray(pages, lines, columns);
 
+    //Displays the book or 3D Array before and after sorting
     cout << "Before Sort" << endl;
-    print3DArray(book, pages, lines, columns);
+    display3DArray(book, pages, lines, columns);
 
     cout << "===============================================" << endl << endl;
 
     cout << "After Sort" << endl;
     sort(book, pages, lines, columns);
-    print3DArray(book, pages, lines, columns);
+    display3DArray(book, pages, lines, columns);
 
+    //Cleans out 3D Array
     delete3DArray(book, pages, lines);
     return 0;
 }
-
+/**
+ *  Dynamically creates a new 3D array and assigns a random number to it
+ * 
+ *  @param(p: Total number of pages in the book
+ *         l: Total number of lines/rows in a page
+ *         c: Total number of columns in a line)
+ *  @return: Returns a dynamically created 3D Array
+ **/
 int*** create3DArray(int p, int l, int c){
-    int*** array3D = new int**[p];
+    int*** array = new int**[p];
         for(int i = 0; i < p; i++){
-            array3D[i] = new int*[l];
+            array[i] = new int*[l];
             for(int j = 0; j < l; j++){
-                array3D[i][j] = new int[c];
+                array[i][j] = new int[c];
                 for(int k = 0; k < c; k++){
-                    array3D[i][j][k] = 5 + (rand() % 51); //Assisngs and random number between 5 and 55 inclusive
+                    array[i][j][k] = randomValueGenerator();
                 }
             }
         }
-    return array3D;
+    return array;
 }
 
+/**
+ *  Generates a random integer between FIRST and LAST inclusive, in this case it is 5 to 55 inclusive.
+ * 
+ *  @param: none/void
+ *  @return: Returns a random integer between FIRST (5) AND LAST(55).
+ **/
+int randomValueGenerator(){
+    const int FIRST = 5;
+    const int LAST = 55;
+    const int DIFFERENCE = LAST - FIRST + 1;
 
-void print3DArray(int*** array, int p, int l, int c){
+    return (FIRST + (rand() % DIFFERENCE)); 
+}
+
+/**
+ *  Displays the pages and it's contents of a book in an organized format. Also displays the total sums of
+ *  values
+ * 
+ *  @param(array: 3D array
+ *         p: Total number of pages in the book
+ *         l: Total number of lines/rows in a page
+ *         c: Total number of columns in a line)
+ *  @return: void
+ **/
+void display3DArray(int*** array, int p, int l, int c){
     for(int i = 0; i < p; i++){
-    cout << "Page " << i << ": " << getPageSum(array, i, l, c) << endl;
-    cout << "-----------------------" << endl;
+    cout << "Page " << i << " || Total sum = " << getPageSum(array, i, l, c) << endl;
+    cout << "----------------------------" << endl;
     for(int j = 0; j < l; j++){
         for(int k = 0; k < c; k++){
             cout << array[i][j][k] << ", ";
@@ -82,6 +117,14 @@ void print3DArray(int*** array, int p, int l, int c){
     }
 }
 
+/**
+ *  Goes to each dynamically created array frees up the memory, and point the arrays in each level to nullptr.
+ * 
+ *  @param(array: 3D array
+ *         p: Total number of pages in the book
+ *         l: Total number of lines/rows in a page
+ *  @return: void
+ **/
 void delete3DArray(int*** array, int p, int l){
     for(int i = 0; i < p; i++){
         for(int j = 0; j < l; j++){
@@ -96,6 +139,15 @@ void delete3DArray(int*** array, int p, int l){
     array = nullptr;
 }
 
+/**
+ *  Calculates the total sum of integer values in each page.
+ * 
+ *  @param(array: 3D array
+ *         pageIndex: Page that is accessed to calculate the total sum of integer values
+ *         l: Total number of lines/rows in a page
+ *         c: Total number of columns in a line)
+ *  @return: Returns the total sum of integer values in a given page.
+ **/
 int getPageSum(int*** array, int pageIndex, int l, int c){
     int sum = 0;
     for(int j = 0; j < l; j++){
@@ -106,9 +158,18 @@ int getPageSum(int*** array, int pageIndex, int l, int c){
     return sum;
 }
 
-void sort(int*** array, int totalPageLength, int l, int c){
-    for(int i = 0; i < totalPageLength; i++){
-        for(int j = 0; j < totalPageLength-i-1; j++){
+/**
+ *  Uses bubble sort to sort pages by their total sum of values in each page
+ * 
+ *  @param(array: 3D array 
+ *         p: Total number of pages in the book
+ *         l: Total number of lines/rows in a page
+ *         c: Total number of columns in a line)
+ *  @return: void
+ **/
+void sort(int*** array, int p, int l, int c){
+    for(int i = 0; i < p; i++){
+        for(int j = 0; j < p-i-1; j++){
             if(getPageSum(array, j, l, c) > getPageSum(array, j + 1, l, c)){
                 swap(array[j], array[j+1]);
             }
