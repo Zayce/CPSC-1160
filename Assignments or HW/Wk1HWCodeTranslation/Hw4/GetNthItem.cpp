b524e25c23420358a408c395d041114efc55a124
Zayce <zayced@gmail.com>
2021-10-01 21:23:46 -0700

    File changes and hw4 question 1

---
+++ Assignments or HW/Wk1HWCodeTranslation/Hw4/GetNthItem.cpp
@@ -0,0 +1,53 @@
+/**
+ *  @author Zoella (Zulhelmi) Mohamad
+ **/
+
+#include <iostream>
+
+using namespace std;
+
+int getNthItem(int* arr, int size, int &start, int n);
+int len(int[]);
+
+//TODO:: Fix this horrible mess
+int main(){
+    int arr[] = {4, 5, 3, 2, 1, 6, 7, 0};
+    int* pArr = arr;
+    int s = len(arr);
+    int startIndex = 0;
+    startIndex = 0;
+
+    int desiredIndex;
+    cout << s << endl;
+    cout << "Please enter a value: ";
+    cin >> desiredIndex;
+
+    int value = getNthItem(pArr, s, startIndex, desiredIndex);
+    cout << value << endl;
+    return 0;
+}
+
+int len(int arr[]){
+    int i = 0, sum = 0;
+    while(arr[i] != 0){
+        sum++;
+        i++;
+    }
+    return sum;
+}
+
+int getNthItem(int* arr, int size, int &start, int n){
+    if(start == n){
+        return arr[start];
+    }
+    else if(n > size){
+        return -1;
+    }
+    else if(start == size){
+        return -1;
+    }
+    else{
+        start++;
+        return getNthItem(arr, size, start, n);
+    }
+}
