33e4533c90320a26615ad95c3eafb68c6b330b4e
Zayce <zayced@gmail.com>
2021-10-01 22:54:18 -0700

    mergeInOrder some inmplementation but not finished

---
+++ Assignments or HW/Wk1HWCodeTranslation/Hw4/MergeInOrder.cpp
@@ -0,0 +1,35 @@
+/**
+ *  @author Zoella (Zulhelmi) Mohamad
+ **/
+
+#include <iostream>
+
+using namespace std;
+
+int* mergeInOrder(int* left, int leftSize, int* right, int rightSize);
+
+int main(){
+
+}
+
+int* mergeInOrder(int* left, int leftSize, int* right, int rightSize){
+    int mergedSize = leftSize + rightSize;
+    int* merged = new int[mergedSize];
+
+    if(leftSize == rightSize){
+        return
+    }
+    else{
+        for(int i  = 0, l = 0, r = 0; i < mergedSize; i++){
+            if(left[i] <= right[i]){
+                merged[i] = left[l]
+                l++;
+            }
+            else{
+                merged[i] = right[l];
+                r++;
+            }
+    }
+    }
+
+}
