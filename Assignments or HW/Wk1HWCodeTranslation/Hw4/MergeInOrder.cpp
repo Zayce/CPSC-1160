953c6f652058252e4aed56793f44e82fecdf008b
Zayce <zayced@gmail.com>
2021-10-03 18:05:41 -0700

    VSCode debugging not working

--- Assignments or HW/Wk1HWCodeTranslation/Hw4/MergeInOrder.cpp
+++ Assignments or HW/Wk1HWCodeTranslation/Hw4/MergeInOrder.cpp
@@ -6,30 +6,46 @@
 
 using namespace std;
 
+void displayArray(int* arr, int length);
 int* mergeInOrder(int* left, int leftSize, int* right, int rightSize);
 
 int main(){
+    int leftTemp[] = {2, 3, 6};
+    int* left = leftTemp;
+    int rightTemp[] = {4, 6, 8};
+    int* right = rightTemp;
+
+    int* merge = mergeInOrder(left, 3, right, 3);
+    displayArray(merge, 6);
+
     return 0;
 }
 
-int* mergeInOrder(int* left, int leftSize, int* right, int rightSize){
-    int mergedSize = leftSize + rightSize;
-    int* merged = new int[mergedSize];
 
-    if(leftSize == rightSize){
-        return
+void displayArray(int* arr, int length){
+    cout << "{";
+    for(int i = 0; i < length - 1; i++){
+        cout << arr[i] << ", ";
     }
-    else{
-        for(int i  = 0, l = 0, r = 0; i < mergedSize; i++){
-            if(left[i] <= right[i]){
-                merged[i] = left[l]
-                l++;
-            }
-            else{
-                merged[i] = right[l];
-                r++;
-            }
+    cout << arr[length - 1];
+    cout << "}" << endl;
+}
+
+
+int* mergeInOrder(int* left, int leftSize, int* right, int rightSize){
+    const int mergedSize = leftSize + rightSize;
+    int* merged = new int[mergedSize];
+    for(int i  = 0, l = 0, r = 0;  i < mergedSize; i++){
+        if(left[l] <= right[r]){
+            merged[i] = left[l];
+            l++;
+        }
+        else{
+            merged[i] = right[r];
+            r++;
+        }
     }
-    }
+
+    return merged;
 
 }
