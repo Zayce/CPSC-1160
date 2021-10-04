0d936cfb1c811c5c3441ab8bb722f1f229c9d9c2
Zayce <zayced@gmail.com>
2021-10-04 13:16:11 -0700

    Added *.plist to .gitIgnore and got DigitProduct working

---
+++ Assignments or HW/Wk1HWCodeTranslation/Hw4/DigitProduct.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @author Zoella (Zulhelmi) Mohamad
+ **/
+
+#include <iostream>
+
+using namespace std;
+
+int digitProduct(int value);
+
+int main(){
+    int value;
+
+    cout << "Please enter an integer: ";
+    cin >> value;
+
+    cout << digitProduct(value) << endl;
+    return 0;
+}
+
+int digitProduct(int value){
+    if((value/10) == 0){
+        return value;
+    }
+    else{
+        return ((value%10) * digitProduct(value/10));
+    }
+}
