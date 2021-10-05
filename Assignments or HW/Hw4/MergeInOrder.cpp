3fce873b2eb247a7928ec358bfc3461bee616b0f
Zayce <zayced@gmail.com>
2021-10-04 23:35:39 -0700

    DYNAMIC ARRAY FIXED for merge. DONT DELETE ARRAYs

--- Assignments or HW/Hw4/MergeInOrder.cpp
+++ Assignments or HW/Hw4/MergeInOrder.cpp
@@ -8,16 +8,42 @@ using namespace std;
 
 int* mergeInOrder(int* left, int leftSize, int* right, int rightSize);
 void displayArray(int* arr, int length);
-void deleteArray(int*);
+void deleteArray(int* arr);
 
 /**
- * Main function that tests the mergeInOrder function, displays and then deletes the dynamically created array.
+ * Main function that tests the mergeInOrder function, 
+ * then displays and then deletes the dynamically created array.
  */
 int main(){
-    int left[] = {2, 3, 6};
-    int right[] = {4, 6, 8};
-    int* merge = mergeInOrder(left, 3, right, 3);
-    displayArray(merge, 6);
+
+    // Dynamically created array 
+    int* left = new int[4];
+    left[0] = 2;
+    left[1] = 3;
+    left[2] = 6;
+    left[3] = 54;
+    int* right = new int[5];
+    right[0] = 4;
+    right[1] = 6;
+    right[2] = 8;
+    right[3] = 10;
+    right[4] = 125;
+    
+    //==========
+
+    //Statically created array
+    // int leftTemp[] = {2, 3, 6, 54};
+    // int rightTemp[] = {4, 6, 8, 10, 125};
+    // int* left = leftTemp;
+    // int* right = rightTemp;
+
+    //==========
+
+    int* merge = mergeInOrder(left, 4, right, 5);
+    deleteArray(left);
+    deleteArray(right);
+    
+    displayArray(merge, 9);
     deleteArray(merge);
     return 0;
 }
@@ -36,14 +62,29 @@ int main(){
 int* mergeInOrder(int* left, int leftSize, int* right, int rightSize){
     const int MERGED_SIZE = leftSize + rightSize;
     int* merged = new int[MERGED_SIZE];
-    for(int i  = 0, leftIndex = 0, rightIndex = 0;  i < MERGED_SIZE; i++){
-        if(left[leftIndex] <= right[rightIndex]){
+    bool isLeftLessOrEqualRight, isLeftArrayEnd, isRightArrayEnd;
+    for(int i  = 0, leftIndex = 0, rightIndex = 0; i < MERGED_SIZE; i++){
+        isLeftLessOrEqualRight = (left[leftIndex] <= right[rightIndex]);
+        isLeftArrayEnd = (leftIndex >= leftSize);
+        isRightArrayEnd = (rightIndex >= rightSize);
+
+        if(isLeftArrayEnd){
+            merged[i] = right[rightIndex];
+            rightIndex += 1;
+        }
+        else if(isRightArrayEnd){
             merged[i] = left[leftIndex];
             leftIndex++;
         }
         else{
-            merged[i] = right[rightIndex];
-            rightIndex++;
+            if(isLeftLessOrEqualRight){
+                merged[i] = left[leftIndex];
+                leftIndex++;
+            }
+            else{
+                merged[i] = right[rightIndex];
+                rightIndex++;
+            }
         }
     }
     return merged;
---
+++ Assignments or HW/Hw4/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+int* left = new int[4];
+    left[0] = 2;
+    left[1] = 3;
+    left[2] = 6;
+    left[3] = 54;
+    int* right = new int[5];
+    right[0] = 4;
+    right[1] = 6;
+    right[2] = 8;
+    right[3] = 10;
+    right[4] = 125;
+    deleteArray(left);
+    deleteArray(right);
