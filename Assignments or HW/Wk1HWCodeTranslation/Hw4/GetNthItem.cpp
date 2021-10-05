464898d89cd32c4c7c531732441f14b1f3b27500
Zayce <zayced@gmail.com>
2021-10-04 19:41:06 -0700

    GetNthItem finally working

--- Assignments or HW/Wk1HWCodeTranslation/Hw4/GetNthItem.cpp
+++ Assignments or HW/Wk1HWCodeTranslation/Hw4/GetNthItem.cpp
@@ -9,39 +9,41 @@ using namespace std;
 int getNthItem(int* arr, int size, int &start, int n);
 void displayArray(int* arr, int length);
 
-
+/**
+ * Main function to test getNthItem function. 
+ */
 int main(){
-    int* arr = new arr[7]{4, 5, 3, 2, 1, 6, 7};
-    int s = sizeof(arr) / sizeof(int);
-    int startIndex = 0;
+    int size, startIndex, indexOfInterest;
+    int arr[] = {4, 5, 3, 2, 1, 6, 7};
+    size = 7;
     startIndex = 0;
 
-    int desiredIndex;
-    cout << "Length is " << s << endl;
     cout << "Please enter a value: ";
-    cin >> desiredIndex;
+    cin >> indexOfInterest;
 
-    cout << getNthItem(arr, s, startIndex, desiredIndex) << endl;
+    cout << "The " << indexOfInterest << "th index value is: ";
+    cout << getNthItem(arr, size, startIndex, indexOfInterest) << endl;
     return 0;
 }
 
-int len(int arr[]){
-    int i = 0, sum = 0;
-    while(arr[i] != 0){
-        sum++;
-        i++;
-    }
-    return sum;
-}
 
+/**
+ * Finds the nth item or nth value of an array recursively.
+ * 
+ * @param   arr: array
+ *          size: length/size of array
+ *          start: pass by reference of the starting index
+ *          n: Index of interest that we want to know the vakue of
+ * 
+ * @return  Returns the value of index n
+ *          Returns -1 if indexOfInterest is more than or equal to size.
+ * 
+ **/
 int getNthItem(int* arr, int size, int &start, int n){
     if(start == n){
         return arr[start];
     }
-    else if(n > size){
-        return -1;
-    }
-    else if(start == size){
+    else if(n >= size){
         return -1;
     }
     else{
@@ -50,6 +52,13 @@ int getNthItem(int* arr, int size, int &start, int n){
     }
 }
 
+/**
+ * Displays the array in a nicer format
+ * 
+ * @param   arr: array
+ *          length: length of array
+ * @return  void, but the array values are outputed to the terminal
+ */
 void displayArray(int* arr, int length){
     cout << "{";
     for(int i = 0; i < length - 1; i++){
